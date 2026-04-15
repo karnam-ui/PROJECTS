9443803edce97c0b7f7ebd570e22318a92138bbc
Suhaas <24071A66C9@vnrvjiet.in>
2026-04-15 19:15:56 +0530

    beginning

---
+++ Realtime stock pipeline/realtime-pipeline/src/main/java/com/pipeline/realtime_pipeline/RealtimePipelineApplication.java
@@ -0,0 +1,13 @@
+package com.pipeline.realtime_pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimePipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimePipelineApplication.class, args);
+	}
+
+}
---
+++ Realtime stock pipeline/realtime-pipeline/src/test/java/com/pipeline/realtime_pipeline/RealtimePipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipeline.realtime_pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimePipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
